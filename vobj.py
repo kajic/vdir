3bbb0d7e76c20bfe59b004f0bebb3effd077d856
Robert Kajic <robert@kajic.com>
2013-05-20 23:19:22 +0200

    Add line breaks between methods

--- vobj.py
+++ vobj.py
@@ -9,14 +9,19 @@ class VObj(object):
 
   def __eq__(self, other):
     return not self<other and not other<self
+
   def __ne__(self, other):
     return self<other or other<self
+
   def __gt__(self, other):
     return other<self
+
   def __ge__(self, other):
     return not self<other
+
   def __le__(self, other):
     return not other<self
+
   def __lt__(self, other):
     return id(self)<id(other)
 
